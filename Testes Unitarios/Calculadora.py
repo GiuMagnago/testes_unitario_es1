c5e987b189c59496dfa858c658d68017fa330c96
Giuseppe Chaves Magnago <giuseppecm2014@gmail.com>
2024-02-05 22:28:44 -0300

    Delete Testes Unitarios directory

--- Testes Unitarios/Calculadora.py
+++
@@ -1,9 +0,0 @@
-class Calculadora:
-    def somar(self, a, b):
-        return a + b
-    def subtrait(self, a, b):
-        return a - b
-    def multiplicar(self, a, b):
-        return a*b
-    def dividir(self, a, b):
-        return a/b
